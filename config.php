12603026a3f9b8669771db2e9596e1d1fe90a986
sydo <sy.do@evizi.com>
2026-03-05 13:12:27 +0700

    update config

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'trolley.proxy.rlwy.net',
+    'port' => '35911',
+    'database' => 'railway',
+    'username' => 'root',
+    'password' => 'PzrztEptLGwwCSmLsCUtUhtzIiohNqvn',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'https://lysovietforum-dev.up.railway.app',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' => 
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
+
+
+
+
